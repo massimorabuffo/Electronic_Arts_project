1c40a0f7c4db5d9680407fbda6dd87cca35442ec
Dennis <emanuele.glorioso@gmail.com>
2023-10-18 18:00:55 +0200

    news moved

--- script.js
+++ script.js
@@ -25,37 +25,7 @@ window.addEventListener("resize", initSlider);
 window.addEventListener("load", initSlider);
 // FINE PARTE CAROSELLO
 
-//<p class="button-tabs">Notizie EA</p>
-// function showDivAvatar(){
-//     if(display == 1){
-//       avatar.style.display = 'flex';
-//       display = 0;
-//     }else{
-//       avatar.style.display = 'none';
-//       display = 1;
-//     }
-//   }
-// const buttonEA = document.getElementById('EA')
-// const buttonEAFC = document.getElementById('EA-FC')
-// const buttonApex = document.getElementById('Apex')
-// const buttonSims = document.getElementById('The-Sims')
-// const buttonStarWars = document.getElementById('Star-Wars')
-// const buttonF1 = document.getElementById('F1')
-// const buttonBattlefield = document.getElementById('Battlefield')
-// const buttonNeedForSpeed = document.getElementById('NeedForSpeed')
-// //Riferimento dei tabs-card-container
-// const tabsCardContainerEA = document.getElementsById('EA123')
-// const tabsCardContainerEAFC = document.getElementsById('EA-Sports')
-// const tabsCardContainerApex = document.getElementsById('apex')
-// const tabsCardContainerSims = document.getElementsById('thesims')
-// const tabsCardContainerStarWars = document.getElementsById('StarWars')
-// const tabsCardContainerF1 = document.getElementsById('F1')
-// const tabsCardContainerBattlefield = document.getElementsById('Battlefield')
-// const tabsCardContainerNeedForSpeed = document.getElementsById('NeedForSpeed')
 
-// function showCardEA(){
-//     tabsCardContainerEA.style.display = 'flex'
-// }
 
 
 function mostraCard(id) {
@@ -79,30 +49,4 @@ function chiudiCard() {
 
 
 
-
-// function mostraCard(id) {
-//     chiudiCard();
-
-//     let cards = document.querySelectorAll('.tabsCardContainer')
-//     for (let i = 0; i < cards.length; i++) {
-//         cards[i].style.display = 'none';
-//     }
-    
-//     const cardDaAprire = cards.filter((el)=>{
-//         console.log(el)
-//     })
-    
-//     let cardDaMostrare = document.getElementById(id);
-//     if (cardDaMostrare) {
-//         cardDaMostrare.style.display = 'flex';
-//     }
-
-// }
-
-// function chiudiCard() {
-//     let cards = document.querySelectorAll('.tabsCardContainer')
-//     for (let i = 0; i < cards.length; i++) {
-//         cards[i].style.display = 'none';
-//     }
-// }
 
